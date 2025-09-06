b7f89c213a175154b4e33ca7f088f17f1f97c99c
MillenaQueiroz <millena.queiroz08@gmail.com>
2025-09-06 18:17:05 -0300

    add 76-substring with concatenation on all words solution

---
+++ questões/leetcode_76/76.py
@@ -0,0 +1,35 @@
+from collections import Counter
+
+class Solution:
+    def findSubstring(self, s, words):
+        if not s or not words:
+            return []
+        
+        word_len = len(words[0])
+        total_len = word_len * len(words)
+        word_count = Counter(words)
+        n = len(s)
+        res = []
+
+        for offset in range(word_len):
+            left = right = offset
+            seen = {}
+            count = 0
+            while right + word_len <= n:
+                word = s[right:right + word_len]
+                right += word_len
+                if word in word_count:
+                    seen[word] = seen.get(word, 0) + 1
+                    count += 1
+                    while seen[word] > word_count[word]:
+                        left_word = s[left:left + word_len]
+                        seen[left_word] -= 1
+                        count -= 1
+                        left += word_len
+                    if count == len(words):
+                        res.append(left)
+                else: 
+                    seen.clear()
+                    count = 0
+                    left = right
+        return res
